e358e1824ff5fdf1ca3eeca2fdc87338ba1c1403
niiquash <qua16006@byui.edu>
2026-02-25 21:48:52 -0700

    Add functionality to toggle button icon and answer section

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,30 +1,52 @@
 import { faqs } from "../data/faqs.js";
 
-function getFAQs() {
-  let faqHtml = "";
-  faqs.forEach((faq) => {
-    faqHtml += `
+let faqHtml = "";
+faqs.forEach((faq) => {
+  faqHtml += `
   <div class="faq-list-item">
     <div class="faq-question-and-button">
       <h4 class="faq-question">
         ${faq.question}
       </h4>
       <div class="buttons">
-        <button class="faq-button">
+        <button class="faq-button js-faq-button">
           <img
-            class="faq-plus-button"
             src="assets/images/icon-plus.svg"
             alt="plus button"
           />
         </button>
       </div>
     </div>
-    <p class="faq-answer">
+    <p class="faq-answer js-faq-answer">
       ${faq.answer}
     </p>
   </div>
   `;
-  });
-  document.querySelector(".js-faq-list").innerHTML = faqHtml;
+});
+document.querySelector(".js-faq-list").innerHTML = faqHtml;
+
+function toggleFAQButton(button) {
+  const img = button.querySelector("img");
+
+  if (img.src.includes("icon-plus")) {
+    img.src = "assets/images/icon-minus.svg";
+  } else {
+    img.src = "assets/images/icon-plus.svg";
+  }
 }
-getFAQs();
+
+function toggleAnswerParagraph(button) {
+  const faqItem = button.closest(".faq-list-item");
+  const answer = faqItem.querySelector(".js-faq-answer");
+
+  answer.classList.toggle("show");
+}
+
+const buttons = document.querySelectorAll(".js-faq-button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    toggleFAQButton(button);
+    toggleAnswerParagraph(button);
+  });
+});
