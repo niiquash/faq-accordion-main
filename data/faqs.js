935a1dbd87b76c313400804babadda2a44323627
niiquash <qua16006@byui.edu>
2026-02-25 21:02:36 -0700

    Render faqs using javascript

---
+++ data/faqs.js
@@ -0,0 +1,26 @@
+export const faqs = [
+  {
+    id: 1,
+    question: "What is Frontend Mentor, and how will it help me?",
+    answer:
+      "Frontend Mentor offers realistic coding challenges ot help developers imporve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building",
+  },
+  {
+    id: 2,
+    question: "Is Frontend Mentor free?",
+    answer:
+      "Frontend Mentor offers realistic coding challenges ot help developers imporve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building",
+  },
+  {
+    id: 3,
+    question: "Can I  use Frontend Mentor projects in my portfolio?",
+    answer:
+      "Frontend Mentor offers realistic coding challenges ot help developers imporve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building",
+  },
+  {
+    id: 4,
+    question: "How can I get help if I'm stuck on a challenge?",
+    answer:
+      "Frontend Mentor offers realistic coding challenges ot help developers imporve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building",
+  },
+];
--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,1 +1,30 @@
-console.log("Accordion here!");
+import { faqs } from "../data/faqs.js";
+
+function getFAQs() {
+  let faqHtml = "";
+  faqs.forEach((faq) => {
+    faqHtml += `
+  <div class="faq-list-item">
+    <div class="faq-question-and-button">
+      <h4 class="faq-question">
+        ${faq.question}
+      </h4>
+      <div class="buttons">
+        <button class="faq-button">
+          <img
+            class="faq-plus-button"
+            src="assets/images/icon-plus.svg"
+            alt="plus button"
+          />
+        </button>
+      </div>
+    </div>
+    <p class="faq-answer">
+      ${faq.answer}
+    </p>
+  </div>
+  `;
+  });
+  document.querySelector(".js-faq-list").innerHTML = faqHtml;
+}
+getFAQs();
